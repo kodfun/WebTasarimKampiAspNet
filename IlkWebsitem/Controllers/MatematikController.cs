9048056d0cd0fbcf95a53f837f5773725c693382
KodluYorum <info@kodluyorum.dev>
2021-05-23 16:39:40 +0300

    get query string form

---
+++ IlkWebsitem/Controllers/MatematikController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IlkWebsitem.Controllers
+{
+    public class MatematikController : Controller
+    {
+        // Matematik/Index?sayi1=3&sayi2=4
+        public IActionResult Index(int sayi1 = 0, int sayi2 = 0)
+        {
+            ViewData["Sayi1"] = sayi1;
+            ViewData["Sayi2"] = sayi2;
+            ViewData["Toplam"] = sayi1 + sayi2;
+            return View();
+        }
+    }
+}
---
+++ IlkWebsitem/Controllers/SehirlerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IlkWebsitem.Controllers
+{
+    public class SehirlerController : Controller
+    {
+        // Sehirler/Bilgi/1
+        public IActionResult Bilgi(int id)
+        {
+            string bilgi;
+
+            switch (id)
+            {
+                case 6:
+                    bilgi = "Ankara Türkiye'nin başkentidir.";
+                    break;
+                case 34:
+                    bilgi = "İstanbul Türkiye'nin en kalabalık şehridir.";
+                    break;
+                case 1:
+                    bilgi = "Adana'da adana kebap yiyebilirsiniz.";
+                    break;
+                default:
+                    bilgi = "Böyle bir şehir bulunamadı";
+                    break;
+            }
+
+            return View(model: bilgi);
+        }
+    }
+}
