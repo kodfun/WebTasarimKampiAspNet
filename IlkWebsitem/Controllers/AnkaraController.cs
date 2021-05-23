3ad8d2a17e808a600145f40ebf20c05555d5dad9
KodluYorum <info@kodluyorum.dev>
2021-05-23 14:58:50 +0300

    Add project files.

---
+++ IlkWebsitem/Controllers/AnkaraController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IlkWebsitem.Controllers
+{
+    public class AnkaraController : Controller
+    {
+        // [Controller]/[Action]
+        // Ankara/Nufus
+        public string Nufus()
+        {
+            return "Ankara'nın Nüfusu: 5663000";
+        }
+
+        // Ankara/Ilceler
+        public IActionResult Ilceler()
+        {
+            ViewBag.ToplamIlceAdedi = 25; // controller'dan view'e veri taşıyoruz
+            return View();
+        }
+    }
+}
