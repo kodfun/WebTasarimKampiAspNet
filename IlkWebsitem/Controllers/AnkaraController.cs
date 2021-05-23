f442275345d425ad4f27f58b5aafaa48a977f954
KodluYorum <info@kodluyorum.dev>
2021-05-23 15:54:58 +0300

    result types

--- IlkWebsitem/Controllers/AnkaraController.cs
+++ IlkWebsitem/Controllers/AnkaraController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace IlkWebsitem.Controllers
@@ -12,14 +13,45 @@ namespace IlkWebsitem.Controllers
         // Ankara/Nufus
         public string Nufus()
         {
-            return "Ankara'nın Nüfusu: 5663000";
+            return "Ankara'nın Nüfusu: 5663000"; // düz metin (plain text)
         }
 
         // Ankara/Ilceler
         public IActionResult Ilceler()
         {
             ViewBag.ToplamIlceAdedi = 25; // controller'dan view'e veri taşıyoruz
-            return View();
+            return View(); // html
+        }
+
+        public IActionResult GezilecekYerler()
+        {
+            List<string> yerler = new List<string>()
+            {
+                "Anıtkabir", "Kızılay Meydanı", "Güvenpark", "Kocatepe Camisi", "Hacı Bayram Veli Camisi", "Ankara Kalesi", "Hamamönü"
+            };
+            return View(yerler);
+        }
+
+        // Ankara/GezilecekYerlerJson
+        public IActionResult GezilecekYerlerJson()
+        {
+            List<string> yerler = new List<string>()
+            {
+                "Anıtkabir", "Kızılay Meydanı", "Güvenpark", "Kocatepe Camisi", "Hacı Bayram Veli Camisi", "Ankara Kalesi", "Hamamönü"
+            };
+            return Json(yerler);
+        }
+
+        // Ankara/Goller
+        public IActionResult Goller()
+        {
+            return Content(@"
+<h1>Ankara'nın Gölleri</h1>
+<ul>
+    <li>Mogan Gölü</li>
+    <li>Mavi Göl</li>
+    <li>Eymir Gölü</li>
+</ul>", "text/html", Encoding.UTF8);
         }
     }
 }
