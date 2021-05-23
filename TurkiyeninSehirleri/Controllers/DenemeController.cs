741da905196def457e13ab4324d39a6f5313e41e
KodluYorum <info@kodluyorum.dev>
2021-05-23 17:05:07 +0300

    boş asp.net projesi ve mvc konfigurasyonu

---
+++ TurkiyeninSehirleri/Controllers/DenemeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TurkiyeninSehirleri.Controllers
+{
+    public class DenemeController : Controller
+    {
+        // Deneme/Index
+        public string Index()
+        {
+            return "Merhaba! Bu bir denemedir.";
+        }
+    }
+}
